ce54d4e2e741f9d3a33b6a2d6b49d1d42af27bbe
elbrauq <elbrauq@gmail.com>
2024-12-18 15:22:18 +0800

    clean up

--- prism-frontend/types/index.ts
+++ prism-frontend/types/index.ts
@@ -12,24 +12,3 @@ export interface NavItem {
   isActive?: boolean;
   items?: NavItem[];
 }
-
-export interface NavItemWithChildren extends NavItem {
-  items: NavItemWithChildren[];
-}
-
-export interface NavItemWithOptionalChildren extends NavItem {
-  items?: NavItemWithChildren[];
-}
-
-export interface FooterItem {
-  title: string;
-  items: {
-    title: string;
-    href: string;
-    external?: boolean;
-  }[];
-}
-
-export type MainNavItem = NavItemWithOptionalChildren;
-
-export type SidebarNavItem = NavItemWithChildren;
