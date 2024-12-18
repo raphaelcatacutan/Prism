cbcef4d3e190401acf3b4d9ba6a9ac901598d0b6
raphaelcatacutan <raphaelcatacutan@gmail.com>
2024-12-18 20:09:21 +0800

    added demo table

--- prism-frontend/app/tasks/page.tsx
+++ prism-frontend/app/tasks/page.tsx
@@ -1,19 +1,17 @@
 import {Metadata} from "next"
-import Image from "next/image"
 import {z} from "zod"
 
 import {columns} from "./components/columns"
 import {DataTable} from "./components/data-table"
-import {UserNav} from "./components/user-nav"
 import {taskSchema} from "./data/schema"
+import PageContainer from "@/components/layout/page-container"
 
 export const metadata: Metadata = {
   title: "Tasks",
   description: "A task and issue tracker build using Tanstack Table.",
 }
 
-// Simulate a database read for tasks.
-async function getTasks() {
+function getTasks() {
   const tasks = [
     {
       "id": "TASK-8782",
@@ -716,45 +714,17 @@ async function getTasks() {
       "priority": "low"
     }
   ]
-
   return z.array(taskSchema).parse(tasks)
 }
 
 export default async function TaskPage() {
-  const tasks = await getTasks()
+  const tasks = getTasks()
 
   return (
-    <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/tasks-light.png"
-          width={1280}
-          height={998}
-          alt="Playground"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/tasks-dark.png"
-          width={1280}
-          height={998}
-          alt="Playground"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-        <div className="flex items-center justify-between space-y-2">
-          <div>
-            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
-            <p className="text-muted-foreground">
-              Here&apos;s a list of your tasks for this month!
-            </p>
-          </div>
-          <div className="flex items-center space-x-2">
-            <UserNav />
-          </div>
+      <PageContainer scrollable>
+        <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+          <DataTable data={tasks} columns={columns} showPagination={true} showToolbar={true} />
         </div>
-        <DataTable data={tasks} columns={columns} />
-      </div>
-    </>
+      </PageContainer>
   )
 }
